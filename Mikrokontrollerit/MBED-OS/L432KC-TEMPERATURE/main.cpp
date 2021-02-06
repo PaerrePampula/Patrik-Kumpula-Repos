bcae8f74cef2ab4c12d24b1dc17ac91bf9b3ebd6
Patrik Kumpula <patrik.kump@gmail.com>
2021-02-06 13:36:23 +0200

    A LED indicator added to signify significant temperature increase

--- Mikrokontrollerit/MBED-OS/L432KC-TEMPERATURE/main.cpp
+++ Mikrokontrollerit/MBED-OS/L432KC-TEMPERATURE/main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "Adafruit_SSD1331.h" 
 #include "Adafruit_GFX.h" 
+#include <iterator>
 Adafruit_SSD1331 OLED(D9, D6, D10, D11, NC, D13); // cs, res, dc, mosi, (nc), sck  
 
 //DigitalOut LED(LED1);     // LED1, LED2, LED3 and LED4 are the D13 PB_3 pin in this board and 
@@ -8,6 +9,7 @@ Adafruit_SSD1331 OLED(D9, D6, D10, D11, NC, D13); // cs, res, dc, mosi, (nc), sc
 DigitalOut VCCEN(D3);
 DigitalOut PMODEN(D5);
 AnalogIn tempSensor(A3);
+AnalogOut ledIndicator(A4);
 // Definition of colours on the OLED display
 #define Black 0x0000
 #define Blue 0x001F
@@ -19,9 +21,10 @@ AnalogIn tempSensor(A3);
 #define White 0xFFFF
 
 
-int first = 0;
+float previousTemp = 0;
+float previousSampling [10];
 float tempsTotal = 0.0;
-
+int counter = 0;
 void resetSampling()
 {
     tempsTotal = 0.0;
@@ -44,20 +47,49 @@ float getTemp()
     float flooredAndRounded = floorf(var*100.0)/100.0;
     return flooredAndRounded;
 }
+//A LED will indicate if the current temperature has increased by 1/4 of a Celsius by checking the current temperature against the previous 10 samplings
+void checkLamp()
+{
+    previousSampling[counter] = getTemp();
+    float currentSum = 0;
+    for (int i = 0; i < 10; i++) 
+    {
+        currentSum += previousSampling[i];
+    }
+    previousTemp = getTemp();
+    if (counter < 10)
+    {
+        counter++;
+    }
+    else
+    {
+        counter = 0;
+    }
+
+    ledIndicator = (getTemp() > (currentSum/10)+0.25f) ? true:false;
+
+
+}
 int main()
 {
         VCCEN = 1;
         PMODEN = 1;
         OLED.begin(); // initialization of display object
         OLED.clearScreen();   
+        OLED.setTextColor(Cyan); // colour of text in cyan
+        ledIndicator = 1;
+
     while (true) {
 
-        OLED.setTextColor(Cyan); // colour of text in cyan
-        ThisThread::sleep_for(300ms);
+        checkLamp();
+
         OLED.setCursor(0,0); // cursor is in x=0 and y=0
         OLED.clearScreen();
+
         OLED.printf("Temp %.2lf Celsius\n\r",getTemp());
-        ThisThread::sleep_for(100ms);
+
+        ThisThread::sleep_for(1000ms);
     }
 }
+
 
